d84a4d6f1dac87209a8ca11af5790ba72a9302b7
Zurab <mcgeni2003@mail.ru>
2023-10-30 14:35:38 +0300

    ImpExp

---
+++ first.js
@@ -0,0 +1,7 @@
+export let name = 'zurab'
+export let age = 20
+
+export function plus(a,b){
+    return a + b;
+}
+
---
+++ second.js
@@ -0,0 +1,2 @@
+import {name, age, plus} from './first.js'
+console.log(name, age, plus(2,2));
